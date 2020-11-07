e2dc56efcb3b44d2b35d8648a08146a75c48973f
taka826 <on85.ohno.takamasa@gmail.com>
2020-11-07 16:33:22 +0900

    webpackers修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,6 +5,8 @@
 require ("./jquery3")
 require ("popper")
 require ("bootstrap")
+import 'bootstrap';
+import '../stylesheets/application';
 
 
 require("@rails/ujs").start()
--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,5 +1,24 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+const { environment } = require('@rails/webpacker')
+const { VueLoaderPlugin } = require('vue-loader')
+const vue = require('./loaders/vue')
 
-const environment = require('./environment')
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', vue)
 
-module.exports = environment.toWebpackConfig()
+// Vue.js フル版（Compiler入り）
+environment.config.resolve.alias = { 'vue$': 'vue/dist/vue.esm.js' }
+
+// ここから
+// jQueryとBootstapのJSを使えるように
+const webpack = require('webpack')
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: 'popper.js'
+  })
+)
+// ここまで
+
+module.exports = environment
